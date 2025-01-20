918fb6006d82b788ee44e675416d727998ab426f
ianvitorbes <55716966+ianvitorbes@users.noreply.github.com>
2025-01-20 19:09:09 -0300

    Correção do commit anterior (Teste de adicionar produtos ao carrinho)

--- teste_de_Login.py
+++ teste_de_Login.py
@@ -1,7 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 chrome_service = Service("C:\\WebDriver\\chromedriver.exe")
 
@@ -24,13 +26,22 @@ password.send_keys(Keys.RETURN)
 # Delay para que a página possa carregar
 driver.implicitly_wait(5)
 
-# Verificação de login bem-sucedido / Verificando se o usuario foi redirecionado para a página de listagem
-try:
-    # Verifica se o título de um dos produtos está visível (indicando a página de listagem)
-    product_title = driver.find_element(By.CLASS_NAME, "inventory_item_name")
+# Verificação de login bem sucedido
+if "Swag Labs" in driver.title:
     print("Login bem-sucedido!")
-except:
+else:
     print("Falha no login!")
+
+# Delay para esperar o botão de Adicionar o carrinho ficar disponivel 
+try:
+    add_to_cart = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))
+    )
+    # Adicionando o produto ao carrinho
+    add_to_cart.click()
+    print("Produto adicionado ao carrinho com sucesso!")
+except Exception as e:
+    print(f"Erro ao tentar adicionar o produto ao carrinho: {e}")
 
 # Fechar navegador pressionando Enter
 input("Pressione Enter para fechar o navegador...")
